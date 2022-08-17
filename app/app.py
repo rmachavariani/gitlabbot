7a9b1e20c52c466b1b0804b130b8d4b4261e77ad
rusudanmachavariani <rusudan@elnino.tech>
2022-08-17 14:03:35 +0400

    Edit app

--- app/app.py
+++ app/app.py
@@ -103,7 +103,3 @@ def slack_events():
 def send_message():
     bot = MagentologyBot()
     return bot.send_message()
-
-
-if __name__ == "__main__":
-    flask_app.run(host='0.0.0.0', port=5000, debug=True)
